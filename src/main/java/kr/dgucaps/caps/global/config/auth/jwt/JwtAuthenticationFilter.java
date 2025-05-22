d97ef8910974f66328d5accf58f1c7d760bfed1a
Jinsoo Yoon <floreo1242@gmail.com>
2025-05-22 18:09:37 +0900

    [feat] 권한 처리 개선: AccessDeniedException 핸들러 추가 및 JwtAuthenticationFilter 수정

--- src/main/java/kr/dgucaps/caps/global/config/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/kr/dgucaps/caps/global/config/auth/jwt/JwtAuthenticationFilter.java
@@ -5,9 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kr.dgucaps.caps.domain.member.entity.Role;
+import kr.dgucaps.caps.domain.member.dto.CustomOAuth2User;
+import kr.dgucaps.caps.domain.member.entity.Member;
+import kr.dgucaps.caps.domain.member.repository.MemberRepository;
 import kr.dgucaps.caps.global.config.auth.UserAuthentication;
+import kr.dgucaps.caps.global.error.ErrorCode;
+import kr.dgucaps.caps.global.error.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
@@ -15,15 +20,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String AUTHORIZATION = "Authorization";
-
     private static final String BEARER = "Bearer ";
 
     private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -32,9 +38,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             // 유효성 검증
             jwtProvider.validateAccessToken(token);
             final Long memberId = jwtProvider.getSubject(token);
-            final Role role = jwtProvider.getRole(token);
+            Member member = memberRepository.findById(memberId)
+                    .orElseThrow(() -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
             // 인증 정보 설정
-            setAuthentication(request, memberId, role);
+            setAuthentication(request, member);
         }
         filterChain.doFilter(request, response);
     }
@@ -57,8 +64,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 
-    private void setAuthentication(HttpServletRequest request, Long memberId, Role role) {
-        UserAuthentication authentication = new UserAuthentication(memberId, null, List.of(role));
+    private void setAuthentication(HttpServletRequest request, Member member) {
+        CustomOAuth2User principal = new CustomOAuth2User(member, Map.of(), "id");
+        UserAuthentication authentication = new UserAuthentication(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority(member.getRole().getAuthority()))
+        );
+        System.out.println("💡 GrantedAuthority: " + authentication.getAuthorities());
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }

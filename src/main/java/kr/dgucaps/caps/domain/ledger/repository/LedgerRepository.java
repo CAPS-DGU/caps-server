d16bbdaeb28c47cd9545b0aeed27ee22c13423fc
dvin-22 <dain5835@gmail.com>
2025-12-02 16:31:49 +0900

    revert merge PR#33

--- src/main/java/kr/dgucaps/caps/domain/ledger/repository/LedgerRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.dgucaps.caps.domain.ledger.repository;
-
-import kr.dgucaps.caps.domain.ledger.entity.Ledger;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LedgerRepository extends JpaRepository<Ledger, Long> {
-}

6be0f80eec098eb017f69ab18e8689e448f0dd8b
dvin-22 <154444746+dvin-22@users.noreply.github.com>
2026-07-23 14:10:03 +0900

    refactor: 게시물 수정 API를 PATCH로 변경

--- src/main/java/kr/dgucaps/caps/domain/blog/controller/BlogController.java
+++ src/main/java/kr/dgucaps/caps/domain/blog/controller/BlogController.java
@@ -14,8 +14,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,7 +58,7 @@ public class BlogController implements BlogApi {
 
     // 게시물 수정
     @PreAuthorize("hasAnyRole('COUNCIL', 'PRESIDENT', 'ADMIN')")
-    @PutMapping("/{blogId}")
+    @PatchMapping("/{blogId}")
     public ResponseEntity<SuccessResponse<?>> modifyBlog(
             @Auth Long memberId,
             @PathVariable("blogId") Integer blogId,

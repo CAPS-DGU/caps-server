b4cd5712d1f8fbefee8ccdfa3cd74a448ec1799f
dvin-22 <154444746+dvin-22@users.noreply.github.com>
2026-07-24 13:03:09 +0900

    fix: 블로그 이미지·썸네일도 비로그인 조회 허용

--- src/main/java/kr/dgucaps/caps/domain/blog/repository/BlogImageRepository.java
+++ src/main/java/kr/dgucaps/caps/domain/blog/repository/BlogImageRepository.java
@@ -4,4 +4,6 @@ import kr.dgucaps.caps.domain.blog.entity.BlogImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogImageRepository extends JpaRepository<BlogImage, Integer> {
+
+    boolean existsByFileUrl(String fileUrl);
 }
